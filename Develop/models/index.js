93217d88e7a8fd75571e059fc389305c612d5d96
Amber Lee <lee.amber.alex@gmail.com>
2021-01-10 20:50:57 -0800

    Initial commits.

---
+++ Develop/models/index.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    Exercise: require("./Exercise"),
+    Workout: require("./Workout"),
+    
+  };
---
+++ Develop/public/api.js
@@ -0,0 +1,46 @@
+const API = {
+  async getLastWorkout() {
+    let res;
+    try {
+      // GET route: FIND that gives all workouts
+      res = await fetch("/api/workouts");
+    } catch (err) {
+      console.log(err)
+    }
+    const json = await res.json();
+
+    return json[json.length - 1];
+  },
+  async addExercise(data) {
+    const id = location.search.split("=")[1];
+// PUT route: UPDATE workouts by adding exercises
+    const res = await fetch("/api/workouts/" + id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+  // POST route: INSERT that creates a new workout
+  async createWorkout(data = {}) {
+    const res = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+
+  async getWorkoutsInRange() {
+    const res = await fetch(`/api/workouts/range`);
+    const json = await res.json();
+
+    return json;
+  },
+};
