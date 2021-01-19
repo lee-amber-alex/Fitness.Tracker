78fe8d14f0399126a96adfa3d014f6df79c41e03
Amber Lee <lee.amber.alex@gmail.com>
2021-01-19 10:53:02 -0800

    Added exercise field to type to the workout model.

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -3,16 +3,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
-  },
-  exercise: [
+ 
+  day: { type: Date, default: Date.now() },
+  exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
-    }
-  ]
+      type: String,
+      name: String,
+      duration: Number,
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
