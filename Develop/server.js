769f419efe5ba8640c7c0da38a42a4ac8c2b5ceb
Amber Lee <lee.amber.alex@gmail.com>
2021-01-21 23:32:06 -0800

    Fixed uppercase database name for mongoose connection.

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -13,9 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true, 
   useCreateIndex: true,
 });
 app.use(require("./routes/api-routes"));
