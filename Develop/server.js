cda42b1fccc60156daa72eb45684965d5dab4483
Amber Lee <lee.amber.alex@gmail.com>
2021-01-24 10:07:46 -0800

    Deploying again.

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/stark-forest-74916", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tranquil-temple-97104", {
   useNewUrlParser: true,
   useUnifiedTopology: true, 
   useCreateIndex: true,
